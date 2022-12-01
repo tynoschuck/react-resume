93814abfbfc1d36ca0e631a00196e1e92885e13c
tynoschuck <58395745+tynoschuck@users.noreply.github.com>
2022-12-01 22:43:37 +0000

    favico 3

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -17,12 +17,12 @@ const Page: NextPage<HomepageMeta> = memo(({children, title, description}) => {
         {/* several domains list the same content, make sure google knows we mean this one. */}
         <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical" />
 
-        <link href="/favicon.ico" rel="icon" sizes="any" />
-        <link href="/icon.svg" rel="icon" type="image/svg+xml" />
-        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
+        <link href="/public/favicon.ico" rel="icon" sizes="any" />
+        <link href="/public/icon.svg" rel="icon" type="image/svg+xml" />
+        <link href="/public/apple-touch-icon.png" rel="apple-touch-icon" />
+        <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
         <link href="/site.webmanifest" rel="manifest" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 
 
         {/* Open Graph : https://ogp.me/ */}
