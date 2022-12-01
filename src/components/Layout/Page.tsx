8c559c77664c5d15deeac73c8d52f30f96ab9bcb
tynoschuck <58395745+tynoschuck@users.noreply.github.com>
2022-12-01 22:52:18 +0000

    icons3

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -22,7 +22,7 @@ const Page: NextPage<HomepageMeta> = memo(({children, title, description}) => {
         <link href="/public/apple-touch-icon.png" rel="apple-touch-icon" />
         <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
         <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-        <link href="/site.webmanifest" rel="manifest" />
+        <link href="/public/site.webmanifest" rel="manifest" />
 
 
         {/* Open Graph : https://ogp.me/ */}
