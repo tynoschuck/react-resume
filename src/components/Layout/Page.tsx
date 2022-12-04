036808870100ce3aa158a08075c7a4b7a947c8bb
tynoschuck <58395745+tynoschuck@users.noreply.github.com>
2022-12-04 11:20:19 +0000

    fixed favico

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -18,7 +18,6 @@ const Page: NextPage<HomepageMeta> = memo(({children, title, description}) => {
         <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical" />
 
         <link href="/public/favicon.ico" rel="icon" sizes="any" />
-        <link href="/public/icon.svg" rel="icon" type="image/svg+xml" />
         <link href="/public/apple-touch-icon.png" rel="apple-touch-icon" />
         <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
         <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
